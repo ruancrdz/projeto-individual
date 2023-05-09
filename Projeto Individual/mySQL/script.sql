59300dfb24ec5585a288adb27d8072a13a0239e1
ruancrdz <ruan.montanari@sptech.school>
2023-05-09 12:52:45 -0300

    modelagem_v2 / script_v2

--- Projeto Individual/mySQL/script.sql
+++ Projeto Individual/mySQL/script.sql
@@ -1,63 +1,72 @@
 create database GYM;
 use GYM;
 
-create table dados (
-idDados int primary key auto_increment,
-idade int,
-peso decimal,
-altura decimal,
-caloriaIdeal decimal
-);
-
-desc dados;
-
 create table usuario (
 idUsuario int primary key auto_increment,
 nome varchar(45),
 sobrenome varchar(45),
 email varchar(45),
-senha varchar(45),
-fkDados int,
-constraint fkDados foreign key (fkDados)
-	references dados(idDados)
+senha varchar(45)
 );
 
 desc usuario;
 
+create table dados (
+idDados int auto_increment,
+idade int,
+peso decimal,
+altura decimal,
+caloriaIdeal decimal,
+fkUsuario int,
+constraint fkUsuarioDados foreign key (fkUsuario) references usuario(idUsuario),
+constraint pkComposta primary key (idDados, fkUsuario)
+);
+
+desc dados;
 
 create table refeicoes (
 idRefeicoes int primary key auto_increment,
 nome varchar(45),
 descricao varchar(45),
 calorias decimal,
+dt date,
+-- dt datetime default current_timestamp,
 fkUsuario int,
-constraint fkUsuario foreign key (fkUsuario)
-	references usuario(idUsuario)
+constraint fkUsuarioRefeicoes foreign key (fkUsuario) references usuario(idUsuario)
 );
 
 desc refeicoes;
 
-
 insert into usuario values
-	(null, "Ruan", "Cardozo", "ruan.montanari@sptech.school", "#Gf52610741855", null);
+	(null, "Ruan", "Cardozo", "ruan.montanari@sptech.school", "#Gf52610741855");
     
 select * from usuario;
     
 insert into dados values
-	(null, 18, 80, 168, 2000);
+	(null, 18, 80, 168, 2000, 1);
     
 select * from dados;
-    
+
 insert into refeicoes values
-	(null, "Arroz e Feijao", "Almoco", "200", 1),
-	(null, "Pizza", "Janta", "500", 1);
+	(null, "Arroz e Feijão", "Almoço", "200", "2023-10-02", 1),
+	(null, "Macarrão Alho e Óleo", "Janta", "400", "2023-10-02", 1),
+	(null, "Arroz e Feijão", "Almoço", "400", "2023-10-03", 1),
+	(null, "Macarrão Alho e Óleo", "Janta", "400", "2023-10-03", 1),
+	(null, "Arroz e Feijão", "Almoço", "500", "2023-10-04", 1),
+	(null, "Arroz e Feijão", "Janta", "200", "2023-10-04", 1),
+	(null, "Arroz e Feijão", "Almoço", "200", "2023-10-05", 1),
+	(null, "Arroz e Feijão", "Janta", "200", "2023-10-05", 1),
+	(null, "Arroz e Feijão", "Almoço", "500", "2023-10-06", 1),
+	(null, "Pizza", "Janta", "500", "2023-10-06", 1);
     
 select * from refeicoes;
 
-update usuario set fkDados = 1 where nome = "Ruan";
+-- Error Code: 1175. You are using safe update mode and you tried to update a table without a WHERE that uses a KEY column. 
+-- To disable safe mode, toggle the option in Preferences -> SQL Editor and reconnect.	0.000 sec
 
-UPDATE usuario set nome = concat(nome, " ", sobrenome);
+update usuario set nome = concat(nome, " ", sobrenome);
 
+-- NOME / EMAIL / SENHA / IDADE / PESO / ALTURA / CALORIA IDEAL
 select usuario.nome,
 	   usuario.email,
 	   usuario.senha,
@@ -65,12 +74,30 @@ select usuario.nome,
 	   dados.peso,
 	   dados.altura,
 	   dados.caloriaIdeal
-from usuario join dados on fkDados = idDados;
+from usuario join dados on idUsuario = fkUsuario;
 
-select usuario.nome,
-       dados.caloriaIdeal,
-       refeicoes.nome,
+-- NOME / CALORIA IDEAL / REFEIÇÃO / DESCRIÇÃO DA REFEIÇÃO / CALORIAS DA REFEIÇÃO
+
+select dt as data,
+	   usuarioDados.nome,
+       refeicoes.nome as refeicao,
        refeicoes.descricao,
        refeicoes.calorias
-from usuario join dados on fkDados = idDados join refeicoes on fkUsuario = idUsuario;
-	
+from usuario as usuarioDados join dados on usuarioDados.idUsuario = dados.fkUsuario join refeicoes on refeicoes.fkUsuario = idUsuario where idUsuario = 1;
+
+
+-- DATA /  CALORIA IDEAL / CALORIA TOTAL DO DIA / CALORIA MÁXIMA DO DIA / CALORIA MÍNIMA DO DIA/ CALORIA MÉDIA DO DIA
+select dt as data,
+	   usuario.nome,
+	   dados.caloriaIdeal,
+	   sum(calorias) as caloriaTotal, 
+	   max(calorias) as caloriaMax, 
+	   min(calorias) as caloriaMin, 
+	   round(avg(calorias)) as caloriaMedia 
+from refeicoes join usuario on refeicoes.fkUsuario = idUsuario join dados on dados.fkUsuario = idUsuario where idUsuario = 1 group by dt;
+
+
+-- REFEIÇÃO // CALORIAS
+select refeicoes.nome,
+	   refeicoes.calorias
+from refeicoes group by nome;
